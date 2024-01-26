16837b2134452416d1faf6b36756c8292d9e7fce
Peter Smazhko <smazhko@gmail.com>
2024-01-27 00:27:21 +0300

    seminar 4(class): исключения (чтение, запись файла, проверка данных-собственные исключения)

--- seminar4/src/main/java/logpass/Main.java
+++ seminar4/src/main/java/logpass/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Проверка данных пользователя");
         ArrayList<String[]> allusers = LogPassReader.loadData();
         for( String[] userinfo: allusers){
             System.out.println(LogPassReader.checkInputData(userinfo));
